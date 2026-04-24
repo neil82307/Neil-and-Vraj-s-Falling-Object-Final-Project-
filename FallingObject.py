944143a19efe198c52a49f240e653444783d2b20
neil82307 <neil82307@gmail.com>
2026-04-24 14:52:52 -0400

    Update FallingObject.py

--- FallingObject.py
+++ FallingObject.py
@@ -20,3 +20,37 @@ COLORS = [
 def main():
     pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 22)
+
+ px = SCREEN_WIDTH // 2
+    py = SCREEN_HEIGHT - PLAYER_SIZE - 10
+    speed = 6
+    player_color = random.choice(COLORS)
+
+    blocks = []
+    spawn_timer = 0
+    block_speed = 4
+
+    score = 0
+    alive = True
+
+ while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                  pygame.quit()
+                sys.exit()
+
+  keys = pygame.key.get_pressed()
+  if alive:
+            if keys[pygame.K_LEFT] and px > 0:
+                px -= speed
+            if keys[pygame.K_RIGHT] and px < SCREEN_WIDTH - PLAYER_SIZE:
+                px += speed
+            if keys[pygame.K_UP] and py > 0:
+                py -= speed
+            if keys[pygame.K_DOWN] and py < SCREEN_HEIGHT - PLAYER_SIZE:
+                py += speed
+
+spawn_timer += 1
+        if spawn_timer > 25:
